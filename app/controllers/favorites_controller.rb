63650c008105fb5e8d8e901caf643f50ee3e2836
tanakaryo <ch5cxcxxh@gmail.com>
2020-04-18 11:45:40 +0900

    favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,23 @@
 class FavoritesController < ApplicationController
   def creat
     @book = Book.find(params[:book_id])
-    @book.favorite(current_user)
+    unless @book.favorite?(current_user)
+      @book.favorite(current_user)
+      respond_to do |format|
+        format.html {redirect_to request.referrer || root_url}
+        format.js
+      end
+    end
   end
 
   def destroy
     @book = Favorite.find(params[:id]).book
-    @book.unfavorite(current_user)
+    if @book.favorite?(current_user)
+      @book.unfavorite(current_user)
+      respond_to do |format|
+        format.html {redirect_to request.referrer || root_url}
+        format.js
+      end
+    end
   end
 end
