929b03f133af6e306aec88c36fe0eb89f11c2d55
tanakaryo <ch5cxcxxh@gmail.com>
2020-04-20 01:58:34 +0900

    update follow

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,12 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+  resources :relationships, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   get 'home/about' => 'homes#about'
 end
 
