07fdc0cf27f38255aa7c17f0330391540b40a5ad
tanakaryo <ch5cxcxxh@gmail.com>
2020-04-20 15:41:27 +0900

    finish follow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,20 @@ class UsersController < ApplicationController
   	end
   end
 
+  def following
+    @title = "Follow Users"
+    @user = User.find(params[:id])
+    @users = @user.following.all
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Follower Users"
+    @user = User.find(params[:id])
+    @users = @user.followers.all
+    render 'show_follow'
+  end
+
   private
   def user_params
   	params.require(:user).permit(:name, :introduction, :profile_image)
