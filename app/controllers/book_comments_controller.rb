0f608b18f307311ec0b310a379cfad07528543bb
tanakaryo <ch5cxcxxh@gmail.com>
2020-04-21 18:49:04 +0900

    update ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,12 +4,12 @@ class BookCommentsController < ApplicationController
     @book_comment = @book.book_comments.build(book_comment_params)
     @book_comment.user = current_user
     @book_comment.save
-      redirect_back(fallback_location: book_url(@book.id))
+    #redirect_back(fallback_location: book_url(@book.id))
     # if @book_comment.save
         # redirect_back(fallback_location: book_url(@book.id))
     # else
       # @book = Book.new
-      # render 'books/show'
+    render :index
     # end
   end
 
@@ -17,7 +17,8 @@ class BookCommentsController < ApplicationController
     @book = Book.find(params[:book_id])
     @book_comment = @book.book_comments.find(params[:id])
     @book_comment.destroy
-    redirect_back(fallback_location: book_path(@book.id))
+    #redirect_back(fallback_location: book_path(@book.id))
+    render :index
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,14 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.build(book_id: params[:book_id])
     favorite.save
-    redirect_back(fallback_location: book_url(@book.id))
+    render :index
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
     favorite.destroy
-    redirect_back(fallback_location: book_url(@book.id))
+    #render 'destroy.js.erb'
+    render :index
   end
 end
